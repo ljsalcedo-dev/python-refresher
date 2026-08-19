b5bcdb5dd5673ff838777df85e2a9ca72fe3ab36
ljsalcedo-dev <ljsalcedo.dev@gmail.com>
2026-08-19 22:47:48 +0800

    feat: basic output

---
+++ the_basics/output.py
@@ -0,0 +1,23 @@
+# displaying an output to the terminal 
+
+print("Hello, World! I'm Lance.")
+print("I am currently doing a python refresher.")
+print("This is a very exciting experience.")
+
+# displaying text
+
+print("This works.") # using double quotes
+print('This also works.') # or single quotes
+
+# print(This is a text) # returns an error since text must be enclosed within single or double quotes
+
+# displaying text within the same line
+
+print("This is a sentence.", end=" ")
+print("This will print in the same line.")
+
+# displaying numbers
+
+print(4)
+print(1+3) # we can also do arithmetic
+print("My favorite number is", 4, "because it's my birthday.") # mix text and numbers (spaces are automatically added between print parameters)
