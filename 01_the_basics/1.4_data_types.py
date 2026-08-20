f32f2b6095e7fde8df633b462a1ab838e581596c
ljsalcedo-dev <ljsalcedo.dev@gmail.com>
2026-08-20 16:19:12 +0800

    feat: data types

---
+++ 01_the_basics/1.4_data_types.py
@@ -0,0 +1,33 @@
+# text type
+x = "Hello World!" # str
+
+# numeric types
+x = 4 # int
+x = 4.44 # float
+x = 4j # complex
+
+# sequence types
+x = ["apple", "banana", "carrot"] # list
+x = ("apple", "banana", "carrot") # tuple
+x = range(4) # range
+
+# mapping type
+x = {
+    "name": "Lance",
+    "age": 23
+} # dict
+
+# set types
+x = {"apple", "banana", "carrot"} # set
+x = frozenset({"apple", "banana", "carrot"}) # frozenset
+
+# boolean type
+x = True # bool
+
+# binary types
+x = b"Hello" # bytes
+x = bytearray(4) # bytearray
+x = memoryview(bytes(4)) # memoryview
+
+# none type = NoneType
+x = None
